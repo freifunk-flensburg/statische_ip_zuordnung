9874a5a0975be35e3d7815b01ba228226513a4be
René Krick <rene.krick@gmx.de>
2014-06-03 22:43:51 +0200

    Update refresh_static_ip.sh

--- refresh_static_ip.sh
+++ refresh_static_ip.sh
@@ -19,8 +19,8 @@ revision_new=$(getCurrentVersion)
 
 if [ "$revision_old" != "$revision_new" ]
 then
-	cat /etc/dhcp/dhcpd.conf.bak >/etc/dhcp/dhcpd.conf
-	cat ./static_ips >>/etc/dhcp/dhcpd.conf
+	/bin/cat /etc/dhcp/dhcpd.conf.bak >/etc/dhcp/dhcpd.conf
+	/bin/cat ./static_ips >>/etc/dhcp/dhcpd.conf
 	/usr/sbin/service isc-dhcp-server restart
 fi
 
