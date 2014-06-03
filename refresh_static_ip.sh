efc757e6473a51fe5815c9b0069d15d0ef39db55
René Krick <rene.krick@gmx.de>
2014-06-03 23:10:44 +0200

    Update refresh_static_ip.sh

--- refresh_static_ip.sh
+++ refresh_static_ip.sh
@@ -1,11 +1,25 @@
 #!/bin/sh
 
 # hop into correct directory to avoid cron pwd sucks
-cd dirname $0
+cd $(dirname $0)
+
+# function to get the current sha-1
+getCurrentVersion() {
+  git log --format=format:%H -1
+}
+
+# get sha-1 before pull
+revision_current=$(getCurrentVersion)
 
 git pull -q
 
-/bin/cat /etc/dhcp/dhcpd.conf.bak >/etc/dhcp/dhcpd.conf
-/bin/cat ./static_ips >>/etc/dhcp/dhcpd.conf
-/etc/init.d/isc-dhcp-server restart
+# get sha-1 after pull
+revision_new=$(getCurrentVersion)
 
+
+if [ "$revision_current" != "$revision_new" ]
+then
+	/bin/cat /etc/dhcp/dhcpd.conf.bak >/etc/dhcp/dhcpd.conf
+	/bin/cat ./static_ips >>/etc/dhcp/dhcpd.conf
+	/usr/sbin/service isc-dhcp-server restart
+fi
